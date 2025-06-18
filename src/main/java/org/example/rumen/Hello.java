14315684599b3d8ef527df90a999215d90944ffa
wesynth <oneruri9527@ggmail.com>
2025-06-18 09:46:12 +0800

    Java学习--基础入门--初始化项目

---
+++ src/main/java/org/example/rumen/Hello.java
@@ -0,0 +1,12 @@
+package org.example.rumen;
+
+import java.util.Arrays;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello.main");
+        System.out.println("args = " + Arrays.toString(args));
+        System.out.println("args = " + args.length);
+    }
+}
